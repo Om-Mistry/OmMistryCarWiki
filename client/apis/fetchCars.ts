e4f686ba1bdccaa1a4432941bf95bbff6eea6aa3
Om Mistry <ommistry03@gmail.com>
2024-08-19 10:52:24 +1200

    First iteration of fetchCars.ts has been created

--- client/apis/fetchCars.ts
+++ client/apis/fetchCars.ts
@@ -12,13 +12,3 @@ export async function fetchCars(): Promise<carLayout[]> {
     throw error
   }
 }
-
-export async function fetchCarById(id: number): Promise<carLayout> {
-  try {
-    const response = await request.get(`${urlRoot}/cars/${id}`)
-    return response.body as carLayout
-  } catch (error) {
-    console.error(`Error fetching car with id ${id}:`, error)
-    throw error
-  }
-}
