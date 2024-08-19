6f5b01e504927ec7b5b1ac0a57dc47bee3710a49
Om Mistry <ommistry03@gmail.com>
2024-08-19 19:50:39 +1200

    Fixed the error with carLayout

--- client/apis/getDBCars.ts
+++ client/apis/getDBCars.ts
@@ -1,7 +1,7 @@
 import request from 'superagent'
 import { carLayout } from '../../models/carLayout.ts'
 
-export async function getDBCars(): carLayout {
+export async function getDBCars(): Promise<carLayout> {
   const response = await request.get('GET/API/V1/cars')
   return response.body
 }
