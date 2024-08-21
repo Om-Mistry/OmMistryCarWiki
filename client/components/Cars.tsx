700a786554a8601ade2173c933b76c23638a4d0c
Om Mistry <ommistry03@gmail.com>
2024-08-21 18:24:49 +1200

    Made some more progress on the API

--- client/components/Cars.tsx
+++ client/components/Cars.tsx
@@ -1,24 +1,37 @@
 import { useEffect, useState } from 'react'
-import { getDBCars } from '../apis/getDBCars'
+import { carLayout } from '../../models/carLayout'
+import { getCars } from '../../db'
 
 export default function DBCars() {
-  const [cars, setCars] = useState([])
-  const [carsLoading, setCarsLoading] = useState(true)
-  const [error, setError] = useState(null)
-
+  const [cars, setCars] = useState<carLayout[]>([])
 
   useEffect(() => {
     const fetchCarData = async () => {
-      try {
-        const data = await getDBCars()
-        setCars(data)
-      } catch (error) {
-        setError(error.message)
-      } finally {
-        setLoading(false)
-      }
+      const carData: carLayout[] = await getCars()
+      setCars(carData)
     }
     fetchCarData()
   }, [])
 
+  console.log(cars)
+
+  return (
+    <>
+      <h2>My Car List</h2>
+      <ul>
+        {cars.map((car) => (
+          <li key={car.id}>
+            <h3>
+              {car.make} {car.model}
+            </h3>
+            <img
+              src={car.image_url}
+              alt={`${car.make} ${car.model}`}
+              style={{ width: '300px' }}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
