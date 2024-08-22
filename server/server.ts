b87d61d71f203522857886cbae9b15ca49f6d32e
Om Mistry <ommistry03@gmail.com>
2024-08-22 17:59:58 +1200

    Testing my server file to see if it all works together

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,16 @@
-import * as Path from 'node:path'
 import express from 'express'
 import { getCars } from '../db'
+
+const server = express()
+server.use(express.json())
+
+server.get('/api/v1/cars', async (req, res) => {
+  try {
+    const cars = await getCars()
+    res.json(cars)
+  } catch (err) {
+    res.status(500).json({ error: 'Cannot get cars' })
+  }
+})
+
+export default server
