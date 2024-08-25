4085e5ae5b86b95468d457448923a18418e46d09
Om Mistry <ommistry03@gmail.com>
2024-08-25 15:41:17 +1200

    Fixed the getDBCars file a bit

--- client/apis/getDBCars.ts
+++ client/apis/getDBCars.ts
@@ -1,10 +1,18 @@
 import request from 'superagent'
 import { carLayout } from '../../models/carLayout.ts'
 
-export async function getDBCars(): Promise<carLayout> {
-  const response = await request.get('GET/API/V1/cars')
+export async function getDBCars(): Promise<carLayout[]> {
+  const response = await request.get('/api/v1/cars')
   return response.body
 }
 
 const data = getDBCars()
 console.log(data)
+
+getDBCars()
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((error) => {
+    console.error('Error getting the awesome cars', error)
+  })
