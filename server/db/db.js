62740bf2c3d5389540321d2882099ed131aa0548
Om Mistry <ommistry03@gmail.com>
2024-08-25 15:14:49 +1200

    Attempted to clean up and polish the db.js file a bit

--- server/db/db.js
+++ server/db/db.js
@@ -12,18 +12,17 @@ export function close() {
   db.destroy()
 }
 
-export function deleteCarsDB(id, db = connection) {
+export function deleteCarsDB(id) {
   const result = db('cars').where({ id }).del()
   return result
 }
 
-export function addCarsDB(string) {
-  const result = db('cars').insert({ task: string })
+export function addCarsDB(car) {
+  const result = db('cars').insert(car)
   return result
 }
 
-export function editCarsDB(id, string) {
-  console.log(string)
-  const result = db('cars').where('id', id).update({ task: string })
+export function editCarsDB(id, updatedCar) {
+  const result = db('cars').where('id', id).update(updatedCar)
   return result
 }
