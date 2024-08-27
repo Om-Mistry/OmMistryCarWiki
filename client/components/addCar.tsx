44cf05396360626422ab104384887abdb57dddf3
Om Mistry <ommistry03@gmail.com>
2024-08-27 17:00:21 +1200

    Going really good with progress

--- client/components/addCar.tsx
+++ client/components/addCar.tsx
@@ -13,4 +13,45 @@ export default function AddCar() {
     image_url: '',
   })
 
+  const mutation = useMutation({
+    mutationFn: (newCar: Omit<carLayout, 'id'>) => addCarDB(newCar),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cars'] })
+    },
+  })
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    mutation.mutate(newCar)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newCar.make}
+        onChange={(e) => setNewCar({ ...newCar, make: e.target.value })}
+        placeholder="Car Make"
+      />
+      <input
+        type="text"
+        value={newCar.model}
+        onChange={(e) => setNewCar({ ...newCar, model: e.target.value })}
+        placeholder="Car Model"
+      />
+      <input
+        type="text"
+        value={newCar.year}
+        onChange={(e) => setNewCar({ ...newCar, year: e.target.value })}
+        placeholder="Car Year"
+      />
+      <input
+        type="text"
+        value={newCar.image_url}
+        onChange={(e) => setNewCar({ ...newCar, image_url: e.target.value })}
+        placeholder="Car Image URL"
+      />
+      <button type="submit">Add a brand new car!</button>
+    </form>
+  )
 }
