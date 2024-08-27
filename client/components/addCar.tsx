7b2eeb4f4263c67d534617225fde2489a5bd67bf
Om Mistry <ommistry03@gmail.com>
2024-08-27 12:50:08 +1200

    Made some progress on addCar

--- client/components/addCar.tsx
+++ client/components/addCar.tsx
@@ -5,4 +5,12 @@ import { carLayout } from '../../models/carLayout'
 
 export default function AddCar() {
   const queryClient = useQueryClient()
+
+  const [newCar, setNewCar] = useState<Omit<carLayout, 'id'>>({
+    make: '',
+    model: '',
+    year: '',
+    image_url: '',
+  })
+
 }
