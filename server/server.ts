247299306de5f7ec3daceb4cc7378a36018fe8cc
Om Mistry <ommistry03@gmail.com>
2024-08-30 12:42:47 +1200

    Fixed the server.ts file had some missing code

--- server/server.ts
+++ server/server.ts
@@ -7,3 +7,13 @@ server.use(express.json())
 server.use('/api/v1/cars', carsRouter)
 
 export default server
+
+import * as Path from 'node:path'
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(Path.resolve('public')))
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
+  })
+}
